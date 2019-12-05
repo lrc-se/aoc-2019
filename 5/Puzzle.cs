da7057f1e9f75871fbf17742822d96f197020f2d
LRC <kabc16@student.bth.se>
2019-12-05 21:16:49 +0100

    Day 5: added examples for new opcodes

--- 5/Puzzle.cs
+++ 5/Puzzle.cs
@@ -20,6 +20,11 @@ namespace AOC2019
       Console.WriteLine("========");
       var runner = new IntcodeRunner(program);
       runner.Run();
+      Console.WriteLine();
+
+      Console.WriteLine("EXAMPLES");
+      Console.WriteLine("========");
+      RunExamples();
     }
 
     public static void RunTests()
@@ -55,6 +60,43 @@ namespace AOC2019
       runner.Program = new List<int>() { 1101, 100, -1, 4, 0 };
       runner.Run();
       Console.WriteLine("Test 7: " + runner.Program.SequenceEqual(new List<int>() { 1101, 100, -1, 4, 99 }));
+    }
+
+    public static void RunExamples() {
+      var runner = new IntcodeRunner();
+      
+      Console.WriteLine("Equal to 8?");
+      runner.Program = new List<int>() { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 };
+      runner.Run();
+
+      Console.WriteLine("Less than 8?");
+      runner.Program = new List<int>() { 3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8 };
+      runner.Run();
+
+      Console.WriteLine("Equal to 8?");
+      runner.Program = new List<int>() { 3, 3, 1108, -1, 8, 3, 4, 3, 99 };
+      runner.Run();
+
+      Console.WriteLine("Less than 8?");
+      runner.Program = new List<int>() { 3, 3, 1107, -1, 8, 3, 4, 3, 99 };
+      runner.Run();
+
+      Console.WriteLine("Non-zero input?");
+      runner.Program = new List<int>() { 3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9 };
+      runner.Run();
+
+      Console.WriteLine("Non-zero input?");
+      runner.Program = new List<int>() { 3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1 };
+      runner.Run();
+
+      Console.WriteLine("Compare to 8");
+      runner.Program = new List<int>()
+      {
+        3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+        1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+        999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+      };
+      runner.Run();
     }
   }
 }
