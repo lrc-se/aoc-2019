44ca96f1055033931f33b58aa286b94c30024afa
LRC <kabc16@student.bth.se>
2019-12-26 20:08:33 +0100

    Day 16: solved first puzzle

--- 16/puzzle.php
+++ 16/puzzle.php
@@ -9,3 +9,9 @@ echo 'Example 1: ' . implode('', $fft->process(str_split('12345678'), 4)) . "\n"
 echo 'Example 2: ' . substr(implode('', $fft->process(str_split('80871224585914546619083218645595'), 100)), 0, 8) . "\n";
 echo 'Example 3: ' . substr(implode('', $fft->process(str_split('19617804207202209144916044189917'), 100)), 0, 8) . "\n";
 echo 'Example 4: ' . substr(implode('', $fft->process(str_split('69317163492948606335995924319873'), 100)), 0, 8) . "\n";
+
+$input = trim(file_get_contents(__DIR__ . '/input.txt'));
+echo "\n";
+echo "PUZZLE 1\n";
+echo "========\n";
+echo 'First eight digits after 100 phases: ' . substr(implode('', $fft->process(str_split($input), 100)), 0, 8) . "\n";
